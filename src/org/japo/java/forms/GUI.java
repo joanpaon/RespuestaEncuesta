2938569523bfca7170f6c810ebf2fa61d73d6803
joanpaon <joanpaon@gmail.com>
2019-04-23 11:58:56 +0200

    :sparkles: Código optimizado

--- src/org/japo/java/forms/GUI.java
+++ src/org/japo/java/forms/GUI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 José A. Pacheco Ondoño - joanpaon@gmail.com.
+ * Copyright 2019 José A. Pacheco Ondoño - joanpaon@gmail.com.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@ import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
-import java.net.URL;
 import java.util.Properties;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -41,38 +40,6 @@ import org.japo.java.libraries.UtilesSwing;
  * @author José A. Pacheco Ondoño - joanpaon@gmail.com
  */
 public final class GUI extends JFrame {
-
-    // Propiedades App
-    public static final String PRP_FAVICON_RESOURCE = "favicon_resource";
-    public static final String PRP_FONT_CONTROL_RESOURCE = "font_control_resource";
-    public static final String PRP_FONT_BORDER_RESOURCE = "font_border_resource";
-    public static final String PRP_FORM_HEIGHT = "form_height";
-    public static final String PRP_FORM_WIDTH = "form_width";
-    public static final String PRP_FORM_TITLE = "form_title";
-    public static final String PRP_IMAGE_HEIGHT = "image_height";
-    public static final String PRP_IMAGE_WIDTH = "image_width";
-    public static final String PRP_IMAGE_YES_RESOURCE = "image_yes_resource";
-    public static final String PRP_IMAGE_NOT_RESOURCE = "image_not_resource";
-    public static final String PRP_IMAGE_MAY_RESOURCE = "image_may_resource";
-    public static final String PRP_QUESTION = "question";
-    public static final String PRP_LOOK_AND_FEEL_PROFILE = "look_and_feel_profile";
-
-    // Valores por Defecto
-    public static final String DEF_FAVICON_RESOURCE = "img/favicon.png";
-    public static final String DEF_FONT_CONTROL_FALLBACK_NAME = Font.DIALOG;
-    public static final String DEF_FONT_CONTROL_SYSTEM_NAME = "Arial";
-    public static final String DEF_FONT_BORDER_FALLBACK_NAME = Font.DIALOG;
-    public static final String DEF_FONT_BORDER_SYSTEM_NAME = "Arial";
-    public static final int DEF_FORM_HEIGHT = 300;
-    public static final int DEF_FORM_WIDTH = 500;
-    public static final String DEF_FORM_TITLE = "Swing Manual App";
-    public static final String DEF_IMAGE_WIDTH = "100";
-    public static final String DEF_IMAGE_HEIGHT = "100";
-    public static final String DEF_IMAGE_YES_RESOURCE = "img/yes.jpg";
-    public static final String DEF_IMAGE_NOT_RESOURCE = "img/not.jpg";
-    public static final String DEF_IMAGE_MAY_RESOURCE = "img/may.jpg";
-    public static final String DEF_QUESTION = "¿Cree Ud. que éste va a ser un buen año?";
-    public static final String DEF_LOOK_AND_FEEL_PROFILE = UtilesSwing.LNF_WINDOWS_PROFILE;
 
     // Referencias
     private final Properties prp;
@@ -117,34 +84,22 @@ public final class GUI extends JFrame {
 
     // Construcción - GUI
     private void initComponents() {
-        // Fuentes
-        fntControl = UtilesSwing.generarFuenteRecurso(prp.getProperty(PRP_FONT_CONTROL_RESOURCE),
-                DEF_FONT_CONTROL_SYSTEM_NAME,
-                DEF_FONT_CONTROL_FALLBACK_NAME);
-        fntBorder = UtilesSwing.generarFuenteRecurso(prp.getProperty(PRP_FONT_BORDER_RESOURCE),
-                DEF_FONT_BORDER_SYSTEM_NAME,
-                DEF_FONT_BORDER_FALLBACK_NAME);
-
         // Etiqueta Pregunta
-        lblPregunta = new JLabel(prp.getProperty(PRP_QUESTION, DEF_QUESTION));
-        lblPregunta.setFont(fntControl.deriveFont(Font.PLAIN, 20f));
+        lblPregunta = new JLabel(prp.getProperty("question"));
         lblPregunta.setHorizontalAlignment(JLabel.CENTER);
 
         // Opción SI
         rbtYes = new JRadioButton("Seguramente");
-        rbtYes.setFont(fntControl.deriveFont(Font.PLAIN, 20f));
         rbtYes.setPreferredSize(new Dimension(200, 30));
         rbtYes.setFocusable(false);
 
         // Opción NO
         rbtNot = new JRadioButton("Dificilmente");
-        rbtNot.setFont(fntControl.deriveFont(Font.PLAIN, 20f));
         rbtNot.setPreferredSize(new Dimension(200, 30));
         rbtNot.setFocusable(false);
 
         // Opción NS/NC
         rbtMay = new JRadioButton("No sabría decir");
-        rbtMay.setFont(fntControl.deriveFont(Font.PLAIN, 20f));
         rbtMay.setPreferredSize(new Dimension(200, 30));
         rbtMay.setSelected(true);
         rbtMay.requestFocus();
@@ -156,22 +111,17 @@ public final class GUI extends JFrame {
         bg.add(rbtNot);
         bg.add(rbtMay);
 
-        // Tamaño Imagen Respuesta
-        ancImgOut = Integer.parseInt(prp.getProperty(PRP_IMAGE_WIDTH, DEF_IMAGE_WIDTH));
-        altImgOut = Integer.parseInt(prp.getProperty(PRP_IMAGE_HEIGHT, DEF_IMAGE_HEIGHT));
-
         // Imagen Respuesta
-        String prpImgOut = prp.getProperty(PRP_IMAGE_MAY_RESOURCE, DEF_IMAGE_MAY_RESOURCE);
-        URL urlImgOut = ClassLoader.getSystemResource(prpImgOut);
-        Image imgOutIni = new ImageIcon(urlImgOut).getImage();
-        Image imgOutFin = imgOutIni.getScaledInstance(ancImgOut, altImgOut, Image.SCALE_FAST);
+        ancImgOut = Integer.parseInt(prp.getProperty("img_answer_width"));
+        altImgOut = Integer.parseInt(prp.getProperty("img_answer_height"));
+        Image imgOut = UtilesSwing.importarImagenRecurso(
+                prp.getProperty("img_may_resource"), ancImgOut, altImgOut);
 
         // Etiqueta Imagen Respuesta
-        lblRespuesta = new JLabel(new ImageIcon(imgOutFin));
+        lblRespuesta = new JLabel(new ImageIcon(imgOut));
 
         // Borde Panel Pregunta
         brdPregunta = new TitledBorder("Pregunta");
-        brdPregunta.setTitleFont(fntBorder.deriveFont(Font.BOLD, 18f));
 
         // Panel Pregunta
         pnlPregunta = new JPanel();
@@ -182,7 +132,6 @@ public final class GUI extends JFrame {
 
         // Borde Panel Opciones
         brdOpciones = new TitledBorder("Opciones");
-        brdOpciones.setTitleFont(fntBorder.deriveFont(Font.BOLD, 18f));
 
         // Panel Opciones
         pnlOpciones = new JPanel();
@@ -197,7 +146,6 @@ public final class GUI extends JFrame {
 
         // Borde Panel Respuesta
         brdRespuesta = new TitledBorder("Respuesta");
-        brdRespuesta.setTitleFont(fntBorder.deriveFont(Font.BOLD, 18f));
 
         // Panel Respuesta
         pnlRespuesta = new JPanel(new GridBagLayout());
@@ -206,39 +154,53 @@ public final class GUI extends JFrame {
         pnlRespuesta.add(lblRespuesta);
 
         // Panel Principal
-        pnlPpal = new JPanel(new BorderLayout(5, 5));
+        pnlPpal.setLayout(new BorderLayout(5, 5));
         pnlPpal.setBorder(new EmptyBorder(10, 10, 10, 10));
         pnlPpal.add(pnlPregunta, BorderLayout.NORTH);
         pnlPpal.add(pnlRespuesta, BorderLayout.EAST);
         pnlPpal.add(pnlOpciones, BorderLayout.CENTER);
 
         // Ventana Principal
-        setContentPane(pnlPpal);
-        setTitle(prp.getProperty(PRP_FORM_TITLE, DEF_FORM_TITLE));
-        try {
-            int height = Integer.parseInt(prp.getProperty(PRP_FORM_HEIGHT));
-            int width = Integer.parseInt(prp.getProperty(PRP_FORM_WIDTH));
-            setSize(width, height);
-        } catch (NumberFormatException e) {
-            setSize(DEF_FORM_WIDTH, DEF_FORM_HEIGHT);
-        }
         setMinimumSize(new Dimension(500, 300));
-        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     // Inicialización Anterior    
     private void initBefore() {
         // Establecer LnF
-        UtilesSwing.establecerLnFProfile(prp.getProperty(
-                PRP_LOOK_AND_FEEL_PROFILE, DEF_LOOK_AND_FEEL_PROFILE));
+        UtilesSwing.establecerLnFProfile(prp.getProperty("look_and_feel_profile"));
+
+        // Fuentes
+        fntControl = UtilesSwing.generarFuenteRecurso(prp.getProperty("font_control_resource"));
+        fntBorder = UtilesSwing.generarFuenteRecurso(prp.getProperty("font_border_resource"));
+
+        // Panel Principal
+        pnlPpal = new JPanel();
+
+        // Ventana Principal
+        setContentPane(pnlPpal);
     }
 
     // Inicialización Posterior
     private void initAfter() {
         // Establecer Favicon
-        UtilesSwing.establecerFavicon(this, prp.getProperty(
-                PRP_FAVICON_RESOURCE, DEF_FAVICON_RESOURCE));
+        UtilesSwing.establecerFavicon(this, prp.getProperty("img_favicon_resource"));
+
+        // Tipografías
+        lblPregunta.setFont(fntControl.deriveFont(Font.PLAIN, 20f));
+        rbtYes.setFont(fntControl.deriveFont(Font.PLAIN, 20f));
+        rbtNot.setFont(fntControl.deriveFont(Font.PLAIN, 20f));
+        rbtMay.setFont(fntControl.deriveFont(Font.PLAIN, 20f));
+        brdPregunta.setTitleFont(fntBorder.deriveFont(Font.BOLD, 18f));
+        brdOpciones.setTitleFont(fntBorder.deriveFont(Font.BOLD, 18f));
+        brdRespuesta.setTitleFont(fntBorder.deriveFont(Font.BOLD, 18f));
+
+        // Ventana Principal
+        setTitle(prp.getProperty("form_title"));
+        int width = Integer.parseInt(prp.getProperty("form_width"));
+        int height = Integer.parseInt(prp.getProperty("form_height"));
+        setSize(width, height);
+        setLocationRelativeTo(null);
 
         // Registro de los Gestores de Eventos
         rbtYes.addActionListener(new AEM(this));
@@ -252,20 +214,17 @@ public final class GUI extends JFrame {
 
         // Selección Recurso Imagen
         if (ae.getSource().equals(rbtYes)) {
-            recurso = prp.getProperty(PRP_IMAGE_YES_RESOURCE, DEF_IMAGE_YES_RESOURCE);
+            recurso = prp.getProperty("img_yes_resource");
         } else if (ae.getSource().equals(rbtNot)) {
-            recurso = prp.getProperty(PRP_IMAGE_NOT_RESOURCE, DEF_IMAGE_NOT_RESOURCE);
+            recurso = prp.getProperty("img_not_resource");
         } else {
-            recurso = prp.getProperty(PRP_IMAGE_MAY_RESOURCE, DEF_IMAGE_MAY_RESOURCE);
+            recurso = prp.getProperty("img_may_resource");
         }
 
-        // Recurso >> URL
-        URL urlRecurso = ClassLoader.getSystemResource(recurso);
-
-        // URL >> Image
-        Image imgRecurso = new ImageIcon(urlRecurso).getImage();
+        // Image ( Original ) >> Image ( Reescalada )
+        Image imgAct = UtilesSwing.importarImagenRecurso(recurso, ancImgOut, altImgOut);
 
         // Image >> JLabel
-        UtilesSwing.escalarImagenEtiqueta(lblRespuesta, imgRecurso, ancImgOut, altImgOut);
+        lblRespuesta.setIcon(new ImageIcon(imgAct));
     }
 }
--- src/org/japo/java/libraries/UtilesSwing.java
+++ src/org/japo/java/libraries/UtilesSwing.java
@@ -209,17 +209,17 @@ public final class UtilesSwing {
         }
     }
 
-    // Escalar Image > Etiqueta
-    public static void escalarImagenEtiqueta(JLabel lblAct, Image imgIni, int ancAct, int altAct) {
-        try {
-            // Imagen Original >> Imagen Escalada 
-            Image imgFin = imgIni.getScaledInstance(ancAct, altAct, Image.SCALE_FAST);
+    // Image ( Tamaño INI ) > Image ( Tamaño FIN )
+    public static Image escalarImagen(Image imgIni, int ancAct, int altAct) {
+        Image imgFin;
 
-            // Icon > Etiqueta Imagen
-            lblAct.setIcon(new ImageIcon(imgFin));
+        try {
+            imgFin = imgIni.getScaledInstance(ancAct, altAct, Image.SCALE_FAST);
         } catch (Exception e) {
-            System.out.println("ERROR: No se ha podido adaptar imagen a etiqueta");
+            imgFin = imgIni;
         }
+
+        return imgFin;
     }
 
     // Portapapeles >> Texto
@@ -241,7 +241,6 @@ public final class UtilesSwing {
             System.out.println("ERROR: Lectura del portapapeles");
         }
 
-        // Texto extraido
         return result;
     }
 
@@ -349,7 +348,6 @@ public final class UtilesSwing {
 
     // Importar Fuente TTF - Fichero
     public static final Font importarFuenteFichero(String fichero) {
-        // Referencia a la fuente
         Font f;
 
         // Cargar Fuente
@@ -359,13 +357,11 @@ public final class UtilesSwing {
             f = null;
         }
 
-        // Devuelve fuente
         return f;
     }
 
     // Importar Fuente TTF - Recurso
     public static final Font importarFuenteRecurso(String recurso) {
-        // Referencia a la fuente
         Font f;
 
         // Cargar Fuente
@@ -375,7 +371,6 @@ public final class UtilesSwing {
             f = null;
         }
 
-        // Devuelve fuente
         return f;
     }
 
@@ -442,11 +437,17 @@ public final class UtilesSwing {
         return fuente;
     }
 
-    // Fuente ( Recurso | Sistema | Lógica ) + Estilo + Talla > Fuente
+    // Fuente ( Recurso | Sistema | Lógica ) > Fuente
     public static final Font generarFuenteRecurso(String recurso,
             String fuenteSistema, String fuenteLogica) {
         return generarFuenteRecurso(recurso, DEF_FONT_STYLE, DEF_FONT_STYLE,
                 fuenteSistema, fuenteLogica);
+    }
+
+    // Fuente ( Recurso ) > Fuente
+    public static final Font generarFuenteRecurso(String recurso) {
+        return generarFuenteRecurso(recurso, DEF_FONT_STYLE, DEF_FONT_STYLE,
+                DEF_FONT_FAMILY, FONT_LOGICAL_SERIF_NAME);
     }
 
     // Campo de texto con DATO + ExpReg + Texto campo vacío
@@ -517,25 +518,47 @@ public final class UtilesSwing {
         return validarCampo(txfActual, UtilesFecha.ER_FECHA, textoCampoVacio);
     }
 
+    // Validar Fuente en las Fuentes del Sistema
     public static final boolean validarFuenteSistema(String fuente) {
         return UtilesArrays.buscar(obtenerTipografiasSistema(), fuente) != -1;
     }
-    
+
+    // Recurso ( String ) > Image
     public static final Image importarImagenRecurso(String recurso) {
-        // Referencia Imagen
         Image img;
-        
+
         try {
             // URL del Recurso
             URL urlPpal = ClassLoader.getSystemResource(recurso);
-            
+
             // Imagen de la URL
             img = new ImageIcon(urlPpal).getImage();
-            
+
         } catch (Exception e) {
             img = new ImageIcon().getImage();
         }
-        
+
+        return img;
+    }
+
+    // Recurso ( String ) > Image ( Reescalada )
+    public static final Image importarImagenRecurso(String recurso, int ancho, int alto) {
+        // Referencia Imagen
+        Image img;
+
+        try {
+            // URL del Recurso
+            URL urlPpal = ClassLoader.getSystemResource(recurso);
+
+            // Imagen de la URL
+            img = new ImageIcon(urlPpal).getImage();
+        } catch (Exception e) {
+            img = new ImageIcon().getImage();
+        }
+
+        // Reescalado de la Imagen
+        img = escalarImagen(img, ancho, alto);
+
         // Devuelve la imagen
         return img;
     }
